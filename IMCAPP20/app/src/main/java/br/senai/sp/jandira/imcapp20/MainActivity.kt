a7c38dd749e82c66f44d0dc7785f07be4fe0858b
HERICK ANDERSON DOS SANTOS <20272523@cfp127aluno.local>
2021-04-15 11:35:59 -0300

    Projetos Android

---
+++ IMCAPP20/app/src/main/java/br/senai/sp/jandira/imcapp20/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.senai.sp.jandira.imcapp20
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        card_imc.setOnClickListener {
+
+            val abrirImc = Intent(this, ImcActivity::class.java)
+            startActivity(abrirImc)
+        }
+    }
+}
