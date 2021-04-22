c41167dc036a26d0374f03764056c3845124166a
HERICK ANDERSON DOS SANTOS <20272523@cfp127aluno.local>
2021-04-22 11:37:03 -0300

    ATualizações

---
+++ IMCAPP20/app/src/main/java/br/senai/sp/jandira/imcapp20/ResulatdoImcActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.imcapp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResulatdoImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resulatdo_imc)
+    }
+}
