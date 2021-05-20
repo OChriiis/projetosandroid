fb5aad9e531b75c2afa02a698f0c87a25d94063d
OChriiis <herickandersondossantos01234@hotmail.com>
2021-05-20 06:21:31 -0300

    Atualizações

--- IMCAPP20/app/src/main/java/br/senai/sp/jandira/imcapp20/NcdActivity.kt
+++ IMCAPP20/app/src/main/java/br/senai/sp/jandira/imcapp20/NcdActivity.kt
@@ -2,10 +2,7 @@ package br.senai.sp.jandira.imcapp20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_ncd.*
 
@@ -15,19 +12,31 @@ class NcdActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ncd)
 
         val btnCalcular: Button = findViewById(R.id.button_calcular_ncd)
-        val spinneridade: Spinner = findViewById(R.id.spinner_idade)
+        val spinnerIdade: Spinner = findViewById(R.id.spinner_idade)
         val spinnerNvlAtv: Spinner = findViewById(R.id.spinner_nvl_atv)
-
         val radiogroup: RadioGroup = findViewById(R.id.radio_group)
-        val id = radiogroup.checkedRadioButtonId;
 
+        val itemSelecionado = radiogroup.checkedRadioButtonId
+        val spinnerSelecionadoNvlAtv = spinnerNvlAtv.selectedItemPosition
+        val spinnerSelecionadoIdade = spinnerIdade.selectedItemPosition
 
         btnCalcular.setOnClickListener {
             if (ed_peso.text.isEmpty()) {
                 ed_peso.error = "Este campo é obrigatório!"
             }
-            if (id == -1){
-                Toast.makeText(applicationContext, "Selecione um sexo!", Toast.LENGTH_SHORT).show();
+            if (itemSelecionado == -1){
+                Toast.makeText(this, "Selecione um sexo!", Toast.LENGTH_SHORT).show()
+            }
+            if (spinnerSelecionadoNvlAtv == 0){
+                Toast.makeText(this, "Selecione um nivel de atividade!", Toast.LENGTH_SHORT).show()
+            }
+            if (spinnerSelecionadoIdade == 0){
+                Toast.makeText(this, "Selecione uma faixa de idade!", Toast.LENGTH_SHORT).show()
+            }
+            if (ed_peso.text.isNotEmpty()) {
+
+                val peso: Double = ed_peso.text.toString().toDouble()
+
             }
         }
 
